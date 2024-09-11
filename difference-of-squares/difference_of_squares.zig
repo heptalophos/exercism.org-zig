2b265e0aa2705e143ff0237e71fa4aa121c03ff8
Zero Delta <zerodeltahedge@gmail.com>
2024-09-11 23:32:04 +0300

    Update difference_of_squares.zig

--- difference-of-squares/difference_of_squares.zig
+++ difference-of-squares/difference_of_squares.zig
@@ -1,14 +1,17 @@
 pub fn squareOfSum(number: usize) usize {
-    _ = number;
-    @compileError("compute the sum of i from 0 to n then square it");
+    var sum: usize = 0;
+    var n: usize = 1;
+    while (n <= number) : (n += 1) sum += n;
+    return sum * sum;
 }
 
 pub fn sumOfSquares(number: usize) usize {
-    _ = number;
-    @compileError("compute the sum of i^2 from 0 to n");
+    var sum: usize = 0;
+    var n: usize = 1;
+    while (n <= number) : (n += 1) sum += n * n;
+    return sum;
 }
 
 pub fn differenceOfSquares(number: usize) usize {
-    _ = number;
-    @compileError("compute the difference between the square of sum and sum of squares");
+    return squareOfSum(number) - sumOfSquares(number);
 }
