9befdd6403c8705114258fec546692d4c95e81de
Zero Delta <zerodeltahedge@gmail.com>
2024-09-19 22:17:12 +0300

    Solve hamming

--- hamming/hamming.zig
+++ hamming/hamming.zig
@@ -1,5 +1,9 @@
-pub fn compute(first: []const u8, second: []const u8) DnaError!usize {
-    _ = first;
-    _ = second;
-    @compileError("please implement the compute function");
+pub const DnaError = error { EmptyDnaStrands, UnequalDnaStrands };
+
+pub fn compute(fst: []const u8, snd: []const u8) DnaError!usize {
+    if (fst.len * snd.len == 0) return DnaError.EmptyDnaStrands;
+    if (fst.len != snd.len) return DnaError.UnequalDnaStrands;
+    var hamming: usize = 0;
+    for (0..fst.len) |i| hamming += if (fst[i] == snd[i]) 0 else 1;
+    return hamming;
 }
