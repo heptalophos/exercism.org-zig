f74b71406a168ea5f207e3c4daccfcf7598dd02f
Zero Delta <zerodeltahedge@gmail.com>
2024-09-26 12:05:52 +0300

    pangram.zig -- passing all

--- pangram/pangram.zig
+++ pangram/pangram.zig
@@ -1,4 +1,13 @@
+const ascii = @import("std").ascii;
+
 pub fn isPangram(str: []const u8) bool {
-    _ = str;
-    @compileError("please implement the isPangram function");
+    var present: u32 = 0;
+    for (str) |ch| {
+        const upch = ascii.toUpper(ch);
+        if ( ascii.isAlphabetic(ch) ) {
+            present |= @as(u32, 1) << @intCast(upch - 'A');
+        }
+        else continue; 
+    }
+    return present == (1 << 26) - 1;
 }
