5107aaa95d9b301668a19caf1fb4fdc06387fe6d
Zero Delta <zerodeltahedge@gmail.com>
2024-09-30 22:28:23 +0300

    square-root solution

--- square-root/square_root.zig
+++ square-root/square_root.zig
@@ -1,20 +1,10 @@
 const math = @import("std").math;
 
 pub fn squareRoot(radicand: usize) usize {
-    const epsilon: f64 = 2.718281828459045;
-    const nat_log: f64 = 
-        99999999 * math.pow(
-            f64, 
-            @floatFromInt(radicand), 
-            (1 / 99999999) - 1
-        );
-    const root = @as(u64, @intFromFloat(math.pow(f64, epsilon, nat_log / 2)));
-    return root;
-    // var r: usize = radicand;
-    // const precision = math.powi(i64, 10.0, -10.0);
-    // while (@abs(radicand - r * r) > precision) {
-    //     r = (r + radicand / r) / 2;
-    // }
-    // return r;
-
- }
+    var candidate: usize = 0;
+    while (candidate < radicand) : (candidate += 1) {
+        if (candidate * candidate != radicand) continue;
+        return candidate;
+    }
+    return radicand;
+}
