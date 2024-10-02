3b6f03030c72fab083a77c64fd8e38a14eeedce2
Zero Delta <zerodeltahedge@gmail.com>
2024-10-02 18:11:39 +0300

    reverse_string.zig - iteration passing all tests

--- reverse-string/reverse_string.zig
+++ reverse-string/reverse_string.zig
@@ -1,6 +1,8 @@
 /// Writes a reversed copy of `s` to `buffer`.
 pub fn reverse(buffer: []u8, s: []const u8) []u8 {
-    _ = buffer;
-    _ = s;
-    @compileError("please implement the reverse function");
+    var i: usize = 1; 
+    while (i <= s.len) : (i += 1) {
+        buffer[i - 1] = s[s.len - i];
+    }
+    return buffer;
 }
