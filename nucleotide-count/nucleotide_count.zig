f6e610284836e1ff57f31b4dc5232c2c9defb4cc
Zero Delta <zerodeltahedge@gmail.com>
2024-10-08 14:07:47 +0300

    nucleotide-count, 1st iteration passing all tests

--- nucleotide-count/nucleotide_count.zig
+++ nucleotide-count/nucleotide_count.zig
@@ -1,3 +1,5 @@
+const ascii = @import("std").ascii;
+
 pub const NucleotideError = error{Invalid};
 
 pub const Counts = struct {
@@ -8,5 +10,16 @@ pub const Counts = struct {
 };
 
 pub fn countNucleotides(s: []const u8) NucleotideError!Counts {
-    _ = s;
+    var cs: Counts = .{ .a = 0, .c = 0, .g = 0, .t = 0};
+    for (s) |n| {
+        const nucl: *u32 = switch (ascii.toUpper(n)) {
+            'A' => &cs.a,
+            'C' => &cs.c,
+            'G' => &cs.g,
+            'T' => &cs.t,
+            else => { return NucleotideError.Invalid; }
+        };
+        nucl.* += 1;
+    }
+    return cs;
 }
