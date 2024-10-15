4a9254c31c5bbd3a67e02057f52e55c4e2c7bef5
Zero Delta <zerodeltahedge@gmail.com>
2024-10-16 01:56:54 +0300

    bob.zig - finally passing all

--- bob/bob.zig
+++ bob/bob.zig
@@ -1,35 +1,23 @@
-const ascii = @import("std").ascii;
+const std = @import("std");
+const ascii = std.ascii;
+const mem = std.mem;
 
-pub fn response(query: []const u8) []const u8 {
-    if (silent(query)) 
-        return "Fine. Be that way!";
-    if (yell(query) and question(query)) 
-        return "Calm down, I know what I'm doing!";
-    if (question(query)) 
-        return "Sure.";
-    if (yell(s)) 
-        return "Whoa, chill out!";
-    return "Whatever.";
+pub fn response(s: []const u8) []const u8 {
+    const query = mem.trim(u8, s, &ascii.whitespace);
+    if (query.len == 0) return "Fine. Be that way!";
+    return  if (query[query.len - 1] == '?')
+                if(!anyUpperz(query)) "Sure."
+                else "Calm down, I know what I'm doing!"
+            else
+                if(!anyUpperz(query)) "Whatever."
+                else "Whoa, chill out!";
 }
 
-fn silent(query: []const u8) bool {
-    for (query) |c| 
-        if (!ascii.isWhitespace(c)) 
-            return false;
-    return true;
-}
-
-fn question(query: []const u8) bool {
-    for (0..query.len) |i| {
-        if (ascii.isWhitespace(s[s.len - i - 1])) 
-            continue;
-        return s[s.len - i - 1] == '?';
-    }
-}
-
-fn yell(query: []const u8) bool {
-    for (query) |c| {
+fn anyUpperz(s: []const u8) bool {
+    var someUpperz = false;
+    for (s) |c| {
         if (ascii.isLower(c)) return false;
-        if (ascii.isUpper(c)) return true;
+        someUpperz = someUpperz or ascii.isUpper(c);
     }
+    return someUpperz;
 }
