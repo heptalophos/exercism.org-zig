1a2caf8a4b96ca29b4767375d42c114d9c9d011e
Zero Delta <zerodeltahedge@gmail.com>
2024-10-31 23:31:29 +0200

    Refactor triangle.zig

--- triangle/triangle.zig
+++ triangle/triangle.zig
@@ -4,30 +4,26 @@ pub const Triangle = struct {
     a: f64, b: f64, c: f64,
 
     pub fn init(a: f64, b: f64, c: f64) TriangleError!Triangle {
-        if (noZeroSides(a, b, c) and triangleInequality(a, b, c))
+        if (2 * @max(a, b, c) < a + b + c and a * b * c > 0)
             return .{.a = a, .b = b, .c = c};
         return TriangleError.Invalid;
     }
 
     pub fn isEquilateral(self: Triangle) bool {
-        return self.a == self.b and self.b == self.c;
+        const min = @min(self.a, self.b, self.c);
+        const max = @max(self.a, self.b, self.c); 
+        return min == max;
     }
 
     pub fn isIsosceles(self: Triangle) bool {
-        return self.a == self.b or self.b == self.c or self.c == self.a;
+        const x, const y, const z = 
+            .{self.a, self.b, self.c};
+        return x == y or y == z or z == x;
     }
 
     pub fn isScalene(self: Triangle) bool {
-        return !self.isIsosceles();
+        const x, const y, const z = 
+            .{self.a, self.b, self.c};
+        return x != y and y != z and z != x;
     }
 };
-
-// Auxiliary fcns
-
-fn noZeroSides( a: f64, b: f64, c: f64) bool { 
-    return a * b * c > 0; 
-}
-
-fn triangleInequality( a: f64, b: f64, c: f64) bool {
-        return 2 * @max(a, b, c) < a + b + c; 
-}
