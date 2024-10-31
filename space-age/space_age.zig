f6c2358bff50bfdd429223b7a300375dc7b1bdf1
Zero Delta <zerodeltahedge@gmail.com>
2024-10-31 23:29:03 +0200

    Solve space-age

--- space-age/space_age.zig
+++ space-age/space_age.zig
@@ -1,9 +1,13 @@
 pub const Planet = enum {
-    mercury,
+    mercury, venus, earth, mars, 
+    jupiter, saturn, uranus, neptune,
 
     pub fn age(self: Planet, seconds: usize) f64 {
-        _ = self;
-        _ = seconds;
-        @compileError("please implement the age method");
+        const earthyear: u64 = 31557600;
+        const orbital_period = [_] f64 { 
+            0.2408467, 0.61519726, 1.0, 1.8808158, 
+            11.862615, 29.447498, 84.016846, 164.79132 };
+        const planet_seconds = orbital_period[@intFromEnum(self)] * earthyear;
+        return @as(f64, @floatFromInt(seconds)) / planet_seconds;
     }
 };
