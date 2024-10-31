6666d8e78b26bd378edc4c45217ecf06b825206f
Zero Delta <zerodeltahedge@gmail.com>
2024-10-31 23:07:35 +0200

    scrabble_score.zig -- first iteration passing all

--- scrabble-score/scrabble_score.zig
+++ scrabble-score/scrabble_score.zig
@@ -1,4 +1,19 @@
+const toUpper = @import("std").ascii.toUpper;
+
 pub fn score(s: []const u8) u32 {
-    _ = s;
-    @compileError("please implement the score function");
+    var total: u32 = 0;
+    for (s) |letter| {
+        total += switch (toUpper(letter)) {
+                    'A', 'E', 'I', 'O', 'U' => 1,
+                    'L', 'N', 'R', 'S', 'T' => 1,
+                    'F', 'H', 'V', 'W', 'Y' => 4,
+                    'B', 'C', 'M', 'P' => 3,
+                    'D', 'G' => 2,
+                    'J', 'X' => 8,
+                    'Q', 'Z' => 10,
+                    'K' => 5,
+                    else => 0,
+        };
+    }
+    return total;
 }
