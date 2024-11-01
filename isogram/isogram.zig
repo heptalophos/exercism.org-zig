8c235058094cb2c98efa9fe490a0ad7a52a9d0ef
Zero Delta <zerodeltahedge@gmail.com>
2024-11-01 23:15:26 +0200

    Solve isogram

--- isogram/isogram.zig
+++ isogram/isogram.zig
@@ -1,4 +1,16 @@
+const std = @import("std");
+const ascii = std.ascii;
+const bitset = std.bit_set;
+
 pub fn isIsogram(str: []const u8) bool {
-    _ = str;
-    @compileError("please implement the isIsogram function");
+    var iso = bitset.IntegerBitSet(26).initEmpty();
+    for (str) |c| {
+        if (!ascii.isAlphabetic(c))
+            continue;
+        const idx = ascii.toLower(c) - 'a';
+        if (iso.isSet(idx)) 
+            return false;
+        iso.set(idx);
+    }
+    return true;
 }
