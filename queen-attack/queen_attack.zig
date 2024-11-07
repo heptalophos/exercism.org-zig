63a43408eb78768a4942f76e53be991021f10f77
Zero Delta <zerodeltahedge@gmail.com>
2024-11-07 05:53:10 +0200

    Update queen_attack.zig

--- queen-attack/queen_attack.zig
+++ queen-attack/queen_attack.zig
@@ -3,14 +3,12 @@ pub const QueenError = error{ InitializationFailure };
 pub const Queen = struct {
     rank: i8,
     file: i8,
-
     pub fn init(row: i8, col: i8) QueenError!Queen {
         if (row < 0 or col < 0 or row > 7 or col > 7) {
             return QueenError.InitializationFailure;
         }
         return Queen{.rank = row, .file = col};        
     }
-
     pub fn canAttack(self: Queen, other: Queen) QueenError!bool {
         const drank = @abs(self.rank - other.rank);
         const dfile = @abs(self.file - other.file);
