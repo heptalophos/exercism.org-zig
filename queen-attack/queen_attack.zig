64bcae8074ff6b3aff92e938d8feb030f0a74be8
Zero Delta <zerodeltahedge@gmail.com>
2024-11-07 05:51:36 +0200

    queen-attack, passing all tests

--- queen-attack/queen_attack.zig
+++ queen-attack/queen_attack.zig
@@ -1,17 +1,19 @@
-pub const QueenError = error{
-    InitializationFailure,
-};
+pub const QueenError = error{ InitializationFailure };
 
 pub const Queen = struct {
+    rank: i8,
+    file: i8,
+
     pub fn init(row: i8, col: i8) QueenError!Queen {
-        _ = row;
-        _ = col;
-        @compileError("please implement the init method");
+        if (row < 0 or col < 0 or row > 7 or col > 7) {
+            return QueenError.InitializationFailure;
+        }
+        return Queen{.rank = row, .file = col};        
     }
 
     pub fn canAttack(self: Queen, other: Queen) QueenError!bool {
-        _ = self;
-        _ = other;
-        @compileError("please implement the canAttack method");
+        const drank = @abs(self.rank - other.rank);
+        const dfile = @abs(self.file - other.file);
+        return (drank * dfile == 0) or (drank / dfile == 1);
     }
 };
