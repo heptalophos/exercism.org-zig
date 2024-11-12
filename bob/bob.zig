5f90ebf194637dcae0e76566dcacad424d0ce5bb
Zero Delta <zerodeltahedge@gmail.com>
2024-11-12 22:30:16 +0200

    Refactor bob

--- bob/bob.zig
+++ bob/bob.zig
@@ -5,12 +5,10 @@ const mem = std.mem;
 pub fn response(s: []const u8) []const u8 {
     const query = mem.trim(u8, s, &ascii.whitespace);
     if (query.len == 0) return "Fine. Be that way!";
-    return  if (query[query.len - 1] == '?')
-                if(!anyUpperz(query)) "Sure."
-                else "Calm down, I know what I'm doing!"
-            else
-                if(!anyUpperz(query)) "Whatever."
-                else "Whoa, chill out!";
+    if (query[query.len - 1] == '?')
+        if(!anyUpperz(query)) return "Sure."
+        else return "Calm down, I know what I'm doing!";
+    return  if(anyUpperz(query)) "Whoa, chill out!" else "Whatever.";
 }
 
 fn anyUpperz(s: []const u8) bool {
