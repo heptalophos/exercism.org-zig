50f824cd93b2d61cbd7a9366c8760b600bb68774
Zero Delta <zerodeltahedge@gmail.com>
2024-11-15 19:54:54 +0200

    Update resistor_color_duo.zig

--- resistor-color-duo/resistor_color_duo.zig
+++ resistor-color-duo/resistor_color_duo.zig
@@ -1,4 +1,8 @@
-pub fn colorCode(colors: [2]ColorBand) usize {
-    _ = colors;
-    @compileError("please implement the colorCode function");
+pub const ColorBand = enum(u8) {
+    black, brown, red, orange, yellow, 
+    green, blue, violet, grey, white
+};
+
+pub fn colorCode(colors: [2]ColorBand) u8 {
+    return 10 * @intFromEnum(colors[0]) + @intFromEnum(colors[1]);
 }
