5088dc3fdad0b711254f9086235a9b89ab21e3e7
Zero Delta <zerodeltahedge@gmail.com>
2024-11-25 20:03:34 +0200

    rotational_cipher.zig - 1st passing iteration

--- rotational-cipher/rotational_cipher.zig
+++ rotational-cipher/rotational_cipher.zig
@@ -1,6 +1,22 @@
-pub fn rotate(allocator: mem.Allocator, text: []const u8, shiftKey: u5) mem.Allocator.Error![]u8 {
-    _ = allocator;
-    _ = text;
-    _ = shiftKey;
-    @compileError("please implement the rotate function");
+const mem = @import("std").mem;
+
+pub fn rotate(allocator: mem.Allocator, 
+              text: []const u8, 
+              shiftKey: u5
+             ) mem.Allocator.Error![]u8 
+{
+    const cipher = try allocator.alloc(u8, text.len);
+
+    for (text, 0..) |char, idx| {
+        if ((char | 32 ) < 'a' or (char | 32) > 'z' or shiftKey == 0) {
+            cipher[idx] = char;
+            continue;
+        }
+        const alpha: u8 = if(char >= 'A' and char <= 'Z') 'A' else 'a';
+        var shift = char - alpha;
+        shift += shiftKey;
+        shift %= 26;
+        cipher[idx] = alpha + shift;
+    }
+    return cipher;
 }
