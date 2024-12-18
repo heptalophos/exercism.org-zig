d66d1d2455e010c2f98d59da07de4dee41c41182
Zero Delta <zerodeltahedge@gmail.com>
2024-12-18 19:22:13 +0200

    Update armstrong_numbers.zig

--- armstrong-numbers/armstrong_numbers.zig
+++ armstrong-numbers/armstrong_numbers.zig
@@ -1,10 +1,9 @@
 const math = @import("std").math;
 
 pub fn isArmstrongNumber(num: u128) bool {
-    var ds: u16 = 0;
-    var x: u128 = num;
+    var ds: u16, var x: u128 = .{0, num};
     while (x != 0) : ( x /= 10 ) ds += 1;
-    x = num;
+    x = num; // again
     var sum: u128 = 0;
     while (x != 0) : (x /= 10)
         sum += math.powi(u128, x % 10, ds) 
