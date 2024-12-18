39066c5284270fd66c773d172eed24392a246976
Zero Delta <zerodeltahedge@gmail.com>
2024-12-18 19:21:48 +0200

    Refactor eliuds_eggs.zig

--- eliuds-eggs/eliuds_eggs.zig
+++ eliuds-eggs/eliuds_eggs.zig
@@ -1,4 +1,16 @@
 pub fn eggCount(number: usize) usize {
-    if (number == 0) return 0;
-    return (1 & number) + eggCount(number >> 1);
+    var n: usize = number;
+    var count: usize = 0;
+    while (n > 0) {
+        count += 1;
+        n &= n - 1;
+    }
+    return count;
+    ////////// OR using tail recursion //////////////////////
+    //                                                     //
+    // if (number == 0) return 0;                          //
+    // return (1 & number) + eggCount(number >> 1);        //
+    //                                                     //  
+    // In general though, the loop above is more efficient //
+
 }
