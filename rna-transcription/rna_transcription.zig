8ca9b73dfae83950dc5bb3d655800d8ca56c3a76
Zero Delta <zerodeltahedge@gmail.com>
2025-01-31 18:09:42 +0200

    rna_transcription.zig - passing tests

--- rna-transcription/rna_transcription.zig
+++ rna-transcription/rna_transcription.zig
@@ -1,7 +1,15 @@
-// Import the appropriate standard library and modules
+const mem = @import("std").mem;
+const RNAError = error{OutOfMemory};
 
 pub fn toRna(allocator: mem.Allocator, dna: []const u8) mem.Allocator.Error![]const u8 {
-    _ = allocator;
-    _ = dna;
-    @compileError("please implement the toRna function");
+    const rna = try allocator.alloc(u8, dna.len);
+    for (rna, dna) |*rn, dn|
+        rn.* = switch (dn) {
+            'A' => 'U',
+            'C' => 'G',
+            'G' => 'C',
+            'T' => 'A',
+            else => return RNAError.OutOfMemory,
+        };
+        return rna;
 }
