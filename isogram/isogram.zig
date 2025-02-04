6df7e51c355e243b8210d8aae950210bface936b
Zero Delta <zerodeltahedge@gmail.com>
2025-02-04 23:46:18 +0200

    Reformat isogram.zig

--- isogram/isogram.zig
+++ isogram/isogram.zig
@@ -5,11 +5,9 @@ const bitset = std.bit_set;
 pub fn isIsogram(str: []const u8) bool {
     var iso = bitset.IntegerBitSet(26).initEmpty();
     for (str) |c| {
-        if (!ascii.isAlphabetic(c))
-            continue;
+        if (!ascii.isAlphabetic(c)) continue;
         const idx = ascii.toLower(c) - 'a';
-        if (iso.isSet(idx)) 
-            return false;
+        if (iso.isSet(idx)) return false;
         iso.set(idx);
     }
     return true;
