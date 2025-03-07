96de66c5c04fd528208f7a7175f0bebbecf82eb5
Zero Delta <zerodeltahedge@gmail.com>
2025-03-07 16:29:31 +0200

    Solve sum-of-multiples

--- sum-of-multiples/sum_of_multiples.zig
+++ sum-of-multiples/sum_of_multiples.zig
@@ -1,9 +1,25 @@
-const std = @import("std");
-const mem = std.mem;
+const mem = @import("std").mem;
 
 pub fn sum(allocator: mem.Allocator, factors: []const u32, limit: u32) !u64 {
     _ = allocator;
-    _ = factors;
-    _ = limit;
-    @compileError("please implement the sum function");
+    
+    var total: u64 = 0;
+
+    for (factors, 0..factors.len) |factor, index| {
+        if (factor == 0) continue;
+        var f = factor;
+        while (f < limit) : (f += factor) {
+            var avoid_label = true;
+            if (f % factor == 0) {
+                for (0..index) |i| {
+                    if (f % factors[i] == 0) {
+                        avoid_label = false;
+                        continue;
+                    }
+                }
+            if (avoid_label) total += f;  
+            }
+        }
+    }
+    return total;
 }
